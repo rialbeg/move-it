f490e30b72b699073a3a73ae80b877cc4b21f950
Gabriel Mascarenhas <gsamascarenhas@gmail.com>
2021-02-27 01:02:17 -0300

    1º dia NLW

---
+++ moveit/src/components/ExperienceBar.tsx
@@ -0,0 +1,13 @@
+export function ExperienceBar(){
+    return (
+        <header className="experience-bar">
+            <span>0 xp</span>
+            <div>
+                <div style={{ width: '50%'}}/>
+
+                <span style={{left:'50%'}} className="current-experience">300xp</span>
+            </div>
+            <span>608 xp</span>
+        </header>
+    );
+}
